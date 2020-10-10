c02827d263116c5e956b5f911479d0625e1383de
Jeremy Ottley <Gazaunga@gmail.com>
2020-10-10 17:46:45 -0400

    Create install.rb

---
+++ install.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+home = File.expand_path('~')
+
+Dir['*'].each do |file|
+  next if file =~ /install/ || file =~ /README/
+  target = File.join(home, ".#{file}")
+  if ARGV.first == 'clean'
+    `rm #{target}`
+  else
+    `ln -ns #{File.expand_path file} #{target}`
+  end
+
+end
+
+# git push on commit
+`echo 'git push' > .git/hooks/post-commit`
+`chmod 755 .git/hooks/post-commit`
