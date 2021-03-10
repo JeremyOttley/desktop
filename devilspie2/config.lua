05c992d32c650d3e03caaf7a04d405e9b66ed615
Jeremy Ottley <Gazaunga@gmail.com>
2021-03-10 07:19:13 -0500

    Update config.lua

--- devilspie2/config.lua
+++ devilspie2/config.lua
@@ -8,6 +8,13 @@ if (get_application_name() == "Document Viewer") then
    maximize();
 end
 
+if (get_application_name() == "Evince") then
+   unmaximize();
+   set_window_geometry(300,0,1150,1200);
+   set_on_top();
+   maximize();
+end
+
 if (get_application_name() == "Adobe Acrobat Pro") then
    unmaximize();
    set_window_geometry(400,0,1150,1200);
