f85a5c3e66e734ee7523053feea8977762c939eb
Jeremy Ottley <Gazaunga@gmail.com>
2021-10-21 13:33:21 -0400

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,3 +1,70 @@
+# Proper history etc
+Import-Module PSReadLine
+
+# Produce UTF-8 by default
+# https://news.ycombinator.com/item?id=12991690
+$PSDefaultParameterValues["Out-File:Encoding"] = "utf8"
+
+# https://technet.microsoft.com/en-us/magazine/hh241048.aspx
+$MaximumHistoryCount = 10000;
+
+Set-Alias trash Remove-ItemSafely
+
+function open($file) {
+  invoke-item $file
+}
+
+function explorer {
+  explorer.exe .
+}
+
+function edge {
+  # Old Edge
+  # start microsoft-edge:
+  #
+  # New Chromioum Edge
+  & "${env:ProgramFiles(x86)}\Microsoft\Edge Dev\Application\msedge.exe"
+}
+function settings {
+  start-process ms-setttings:
+}
+
+# Oddly, Powershell doesn't have an inbuilt variable for the documents directory. So let's make one:
+# From https://stackoverflow.com/questions/3492920/is-there-a-system-defined-environment-variable-for-documents-directory
+$env:DOCUMENTS = [Environment]::GetFolderPath("mydocuments")
+
+# PS comes preset with 'HKLM' and 'HKCU' drives but is missing HKCR 
+#New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+
+# Truncate homedir to ~
+function limit-HomeDirectory($Path) {
+  $Path.Replace("$home", "~")
+}
+
+# Must be called 'prompt' to be used by pwsh 
+# https://github.com/gummesson/kapow/blob/master/themes/bashlet.ps1
+function prompt {
+  $realLASTEXITCODE = $LASTEXITCODE
+  Write-Host $(limit-HomeDirectory("$pwd")) -ForegroundColor Yellow -NoNewline
+  Write-Host " $" -NoNewline
+  $global:LASTEXITCODE = $realLASTEXITCODE
+  Return " "
+}
+
+# Make $lastObject save the last object output
+# From http://get-powershell.com/post/2008/06/25/Stuffing-the-output-of-the-last-command-into-an-automatic-variable.aspx
+function out-default {
+  $input | Tee-Object -var global:lastobject | Microsoft.PowerShell.Core\out-default
+}
+
+# If you prefer oh-my-posh
+# Import-Module posh-git
+# Import-Module oh-my-posh
+
+function rename-extension($newExtension){
+  Rename-Item -NewName { [System.IO.Path]::ChangeExtension($_.Name, $newExtension) }
+}
+
 function twitch {
 	[CmdletBinding()]
 	param(
@@ -36,4 +103,46 @@ function instastory {
 	  [string] $Username
 	)
 	python pyinstastories.py -d $Username --no-thumbs
+}
+
+function nimc {
+	[CmdletBinding()]
+	param(
+	  [Parameter()]
+	  [string] $program
+	)
+	nim c $program
+}
+
+function nimr {
+	[CmdletBinding()]
+	param(
+	  [Parameter()]
+	  [string] $program
+	)
+	nim c -r $program
+}
+
+function extract-all {
+	[CmdletBinding()]
+	$files = Get-Childitem $pwd -Recurse -Include *.zip
+        foreach ($file in $files) { 
+          tar -xf $file 
+        }
+}
+
+function repl {
+  clojure -M:repl/rebel
+}
+
+function reveal {
+  clojure -M:repl/rebel-reveal
+}
+
+function cljs {
+  clojure -M:repl/rebel-cljs
+}
+
+function socket {
+  clojure -M:repl/socket
 }
